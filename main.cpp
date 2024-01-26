fe5a93da9e2eb620b21086c3debbb733e3c66026
Vladimir <vladimir_pogonin@hotmail.com>
2024-01-26 12:12:03 +0300

    Some examples were added

--- main.cpp
+++ main.cpp
@@ -30,37 +30,79 @@ struct MyModel {
   serialize::TModelField<double> f9{"Double", {3.8}};
   serialize::TModelField<std::map<int, double>> f10{"Map", {}};
   serialize::TModelField<std::string> f11{"String", {"Hello Thrift"}, false};
-  serialize::TModelField<std::list<int>> f12{"List", {1, 2, 3}, false};
-  serialize::TModelField<std::string_view> f13{"StringView", {}};
+  serialize::TModelField<std::list<int>> f12{"List", {1, 2, 3}, false};  
 };
 
-int main() {
-  auto transWr = std::make_shared<transport::TSimpleFileTransport>(
-      "/tmp/out.json", false, true);
-  auto transRd = std::make_shared<transport::TSimpleFileTransport>(
-      "/tmp/out.json", true, false);
-  auto protoWr = std::make_shared<protocol::TJSONProtocol>(transWr);
-  auto protoRd = std::make_shared<protocol::TJSONProtocol>(transRd);
+void serializeExample(){
+    auto transWr = std::make_shared<transport::TSimpleFileTransport>(
+        "/tmp/out.json", false, true);
 
-  MyModel data;
-  data.f2.SetValue(1);
-  data.f9.SetValue(2.5);
-  data.f10.Value().try_emplace(4, 5);
-  data.f10.Value().try_emplace(6, 7);
-  data.f12.Value().push_back(12);
-  serialize::TPfrSerializer<MyModel> serialzer(protoWr);
-  serialize::TPfrDeserializer<MyModel> deserialzer(protoRd);
+    auto protoWr = std::make_shared<protocol::TJSONProtocol>(transWr);
+    serialize::TPfrSerializer<MyModel> serialzer(protoWr);
 
-  std::array<char,20> temp{};
-  data.f13.SetValue(std::string_view{temp.data(),20});
+    MyModel data;
+    data.f2.SetValue(1);
+    data.f9.SetValue(2.5);
+    data.f10.Value().try_emplace(4, 5);
+    data.f10.Value().try_emplace(6, 7);
+    data.f12.Value().push_back(12);
 
-  auto wr = serialzer.serialize(data);
-  data.f2.Erase();
-  data.f9.Erase();
-  data.f10.Erase();
-  data.f12.Erase();
+    serialzer.serialize(data);
+}
 
-  auto rd = deserialzer.deserialize(data);
+void deserializeExample(){
+    auto transRd = std::make_shared<transport::TSimpleFileTransport>(
+        "/tmp/out.json", true, false);
 
-  return wr == rd;
+    auto protoRd = std::make_shared<protocol::TJSONProtocol>(transRd);
+
+    MyModel data;
+    serialize::TPfrDeserializer<MyModel> deserialzer(protoRd);
+
+    deserialzer.deserialize(data);
+}
+
+
+struct StringViewModel{
+    static constexpr serialize::TModelName name = "";
+    serialize::TModelField<std::string_view> f0{"StringView", {}};
+};
+
+void deserializeStringViewExample(){
+    StringViewModel data;
+
+    const std::array<char, 20> seriaizationMemTemp  {"Hello thrift!"};
+          std::array<char, 20> deseriaizationMemTemp{};
+
+    //Serialization
+    {
+        auto transWr = std::make_shared<transport::TSimpleFileTransport>("/tmp/sv.json", false, true);
+        auto protoWr = std::make_shared<protocol::TJSONProtocol>(transWr);
+
+        serialize::TPfrSerializer<StringViewModel> serialzer(protoWr);
+        data.f0.SetValue(std::string_view{seriaizationMemTemp.data(), 20});
+        serialzer.serialize(data);
+    }
+
+    // Deserialization
+    {
+        auto transRd = std::make_shared<transport::TSimpleFileTransport>("/tmp/sv.json", true, false);
+        auto protoRd = std::make_shared<protocol::TJSONProtocol>(transRd);
+
+        serialize::TPfrDeserializer<StringViewModel> deserialzer(protoRd);
+        data.f0.SetValue(std::string_view{deseriaizationMemTemp.data(), 20});
+        deserialzer.deserialize(data);
+    }
+
+    int compare{};
+}
+
+
+int main()
+{
+    serializeExample();
+    deserializeExample();
+    deserializeStringViewExample();
+
+    return 0;
 }
